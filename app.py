6c0cfec2baf85214d019aaf43dd862ce485e1ac5
svignesh93 <vignesh.vic727@gmail.com>
2022-05-12 01:12:47 +0530

    Stored artist records in db

--- app.py
+++ app.py
@@ -437,16 +437,37 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-  return render_template('pages/home.html')
-
+  error = False
+  try:
+    artist = Artist(
+      name = request.form["name"],
+      city = request.form["city"],
+      state = request.form["state"],
+      phone = request.form["phone"],
+      genres = request.form.getlist("genres"),
+      image_link = request.form["image_link"],
+      facebook_link = request.form["facebook_link"],
+      website_link = request.form["website_link"],
+      seeking_venue = request.form.get("seeking_venue", default=False, type=Boolean),
+      seeking_description = request.form["seeking_description"],
+    )
+    db.session.add(artist)
+    db.session.commit()
+  except:
+    db.session.rollback()
+    error = True
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    # e.g., on unsuccessful db insert, flash an error instead.
+    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+    abort(500)
+  else:
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    return render_template('pages/home.html')
 
 #  Shows
 #  ----------------------------------------------------------------
