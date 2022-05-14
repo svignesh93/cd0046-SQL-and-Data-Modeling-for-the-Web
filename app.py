5157ee29227c3703a62f1ad44a5ee1e0dbf2645b
svignesh93 <vignesh.vic727@gmail.com>
2022-05-14 22:45:01 +0530

    Updated artist search records from db

--- app.py
+++ app.py
@@ -331,18 +331,27 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 1,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+  searchTerm = request.form.get('search_term', '')
+
+  artists = Artist.query.filter(Artist.name.ilike("%" + searchTerm + "%")).all()
+  
+  data = []
+  current_time = datetime.now()
+
+  for artist in artists:
+    upcomingShows = list(filter(lambda show: show.start_time > current_time, artist.shows))
+    data.append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": upcomingShows.count,
+    })
+
+  result={
+    "count": len(artists),
+    "data": data
   }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+
+  return render_template('pages/search_artists.html', results=result, search_term=searchTerm)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
