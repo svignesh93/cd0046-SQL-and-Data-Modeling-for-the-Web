c7e54e574fae48dcbd07dff9ed07abc02e18ae2e
svignesh93 <vignesh.vic727@gmail.com>
2022-05-17 23:33:31 +0530

    Modularized enum classes

---
+++ enums.py
@@ -0,0 +1,87 @@
+from enum import Enum
+
+class States(Enum):
+    AL = 'AL'
+    AK = 'AK'
+    AZ = 'AZ'
+    AR = 'AR'
+    CA = 'CA'
+    CO = 'CO'
+    CT = 'CT'
+    DE = 'DE'
+    DC = 'DC'
+    FL = 'FL'
+    GA = 'GA'
+    HI = 'HI'
+    ID = 'ID'
+    IL = 'IL'
+    IN = 'IN'
+    IA = 'IA'
+    KS = 'KS'
+    KY = 'KY'
+    LA = 'LA'
+    ME = 'ME'
+    MT = 'MT'
+    NE = 'NE'
+    NV = 'NV'
+    NH = 'NH'
+    NJ = 'NJ'
+    NM = 'NM'
+    NY = 'NY'
+    NC = 'NC'
+    ND = 'ND'
+    OH = 'OH'
+    OK = 'OK'
+    OR = 'OR'
+    MD = 'MD'
+    MA = 'MA'
+    MI = 'MI'
+    MN = 'MN'
+    MS = 'MS'
+    MO = 'MO'
+    PA = 'PA'
+    RI = 'RI'
+    SC = 'SC'
+    SD = 'SD'
+    TN = 'TN'
+    TX = 'TX'
+    UT = 'UT'
+    VT = 'VT'
+    VA = 'VA'
+    WA = 'WA'
+    WV = 'WV'
+    WI = 'WI'
+    WY = 'WY'
+
+    @classmethod
+    def choices(cls):
+        """ Methods decorated with @classmethod can be called 
+        statically without having an instance of the class."""
+        return [(choice.name, choice.value) for choice in cls]
+
+class Genres(Enum):
+    Alternative = 'Alternative'
+    Blues =  'Blues'
+    Classical = 'Classical'
+    Country = 'Country'
+    Electronic = 'Electronic'
+    Folk = 'Folk'
+    Funk = 'Funk'
+    HipHop = 'Hip-Hop'
+    HeavyMetal = 'Heavy Metal'
+    Instrumental = 'Instrumental'
+    Jazz = 'Jazz'
+    MusicalTheatre = 'Musical Theatre'
+    Pop = 'Pop'
+    Punk = 'Punk'
+    RAndB = 'R&B'
+    Reggae = 'Reggae'
+    RockNRoll = 'Rock n Roll'
+    Soul = 'Soul'
+    Other = 'Other'
+
+    @classmethod
+    def choices(cls):
+        """ Methods decorated with @classmethod can be called 
+        statically without having an instance of the class."""
+        return [(choice.name, choice.value) for choice in cls]
--- forms.py
+++ forms.py
@@ -1,88 +1,8 @@
-from enum import Enum
 from datetime import datetime
-from flask_wtf import Form
+from enums import States, Genres
+from flask_wtf import FlaskForm as Form
 from wtforms import IntegerField, StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
 from wtforms.validators import NumberRange, DataRequired, URL
-
-class States(Enum):
-    AL = 'AL'
-    AK = 'AK'
-    AZ = 'AZ'
-    AR = 'AR'
-    CA = 'CA'
-    CO = 'CO'
-    CT = 'CT'
-    DE = 'DE'
-    DC = 'DC'
-    FL = 'FL'
-    GA = 'GA'
-    HI = 'HI'
-    ID = 'ID'
-    IL = 'IL'
-    IN = 'IN'
-    IA = 'IA'
-    KS = 'KS'
-    KY = 'KY'
-    LA = 'LA'
-    ME = 'ME'
-    MT = 'MT'
-    NE = 'NE'
-    NV = 'NV'
-    NH = 'NH'
-    NJ = 'NJ'
-    NM = 'NM'
-    NY = 'NY'
-    NC = 'NC'
-    ND = 'ND'
-    OH = 'OH'
-    OK = 'OK'
-    OR = 'OR'
-    MD = 'MD'
-    MA = 'MA'
-    MI = 'MI'
-    MN = 'MN'
-    MS = 'MS'
-    MO = 'MO'
-    PA = 'PA'
-    RI = 'RI'
-    SC = 'SC'
-    SD = 'SD'
-    TN = 'TN'
-    TX = 'TX'
-    UT = 'UT'
-    VT = 'VT'
-    VA = 'VA'
-    WA = 'WA'
-    WV = 'WV'
-    WI = 'WI'
-    WY = 'WY'
-
-    def __str__(self):
-        return str(self.value)
-
-class Genres(Enum):
-    Alternative = 'Alternative'
-    Blues =  'Blues'
-    Classical = 'Classical'
-    Country = 'Country'
-    Electronic = 'Electronic'
-    Folk = 'Folk'
-    Funk = 'Funk'
-    HipHop = 'Hip-Hop'
-    HeavyMetal = 'Heavy Metal'
-    Instrumental = 'Instrumental'
-    Jazz = 'Jazz'
-    MusicalTheatre = 'Musical Theatre'
-    Pop = 'Pop'
-    Punk = 'Punk'
-    RAndB = 'R&B'
-    Reggae = 'Reggae'
-    RockNRoll = 'Rock n Roll'
-    Soul = 'Soul'
-    Other = 'Other'
-
-    def __str__(self):
-        return str(self.value)
 
 class ShowForm(Form):
     artist_id = IntegerField(
@@ -108,7 +28,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=States.__members__.values,
+        choices=States.choices(),
         coerce=str
     )
     address = StringField(
@@ -122,7 +42,7 @@ class VenueForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=Genres.__members__.values,
+        choices=Genres.choices(),
         coerce=str
     )
     facebook_link = StringField(
@@ -145,7 +65,7 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=States.__members__.values,
+        choices=States.choices(),
         coerce=str
     )
     phone = StringField(
@@ -156,7 +76,7 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=Genres.__members__.values,
+        choices=Genres.choices(),
         coerce=str
     )
     facebook_link = StringField(
